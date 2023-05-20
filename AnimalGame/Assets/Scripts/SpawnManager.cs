1f8b65566dcf83b534da626c99ab19368b6c3b57
LeeYunSung <32055099+LeeYunSung@users.noreply.github.com>
2023-05-20 22:55:42 +0900

    [add] Animal Game 완성

--- AnimalGame/Assets/Scripts/SpawnManager.cs
+++ AnimalGame/Assets/Scripts/SpawnManager.cs
@@ -5,12 +5,19 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] animalPrefabs;
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.S)) {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            int randomX = Random.Range(-13, 13);
+    private const float START_DELAY = 2f;
+    private const float SPAWN_INTERVAL = 1.5f;
 
-            Instantiate(animalPrefabs[animalIndex], new Vector3(randomX, 0, 20), animalPrefabs[animalIndex].transform.rotation);
-        } 
+    private void Start() {
+        InvokeRepeating("SpawnAnimal", START_DELAY, SPAWN_INTERVAL);
+    }
+    private void SpawnAnimal() {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        int randomX = Random.Range(-13, 13);
+
+        Instantiate(animalPrefabs[animalIndex],
+            new Vector3(randomX, 0, 20),
+            animalPrefabs[animalIndex].transform.rotation,
+            gameObject.transform);
     }
 }
