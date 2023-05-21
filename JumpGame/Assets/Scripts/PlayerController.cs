34392292806c45df0c706c160b42e7e263b86677
LeeYunSung <dldbstjd6350@naver.com>
2023-05-21 23:47:12 +0900

    [add] 파티클, 사운드 추가

--- JumpGame/Assets/Scripts/PlayerController.cs
+++ JumpGame/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,17 @@ public class PlayerController : MonoBehaviour{
 
     private const float jumpForce = 15f;
     private const float gravityModifier = 3f;
+
+    [SerializeField] private ParticleSystem explosionParticle;
+    [SerializeField] private ParticleSystem dirtParticle;
+
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip crashSound;
+
     private Rigidbody playerRb;
     private Animator playerAnim;
+    private AudioSource playerAudio;
+
     private bool isOnGround = true;
     private bool isGameOver = false;
 
@@ -17,25 +26,38 @@ public class PlayerController : MonoBehaviour{
     void Start(){
         playerRb = transform.GetComponent<Rigidbody>();
         playerAnim = transform.GetComponent<Animator>();
+        playerAudio = transform.GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
     void Update(){
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
             isOnGround = false;
+
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+
+            playerAudio.PlayOneShot(jumpSound, 1f);
         }
     }
 
     private void OnCollisionEnter(Collision collision){
         if(collision.gameObject.CompareTag("Ground")) {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle")) {
             isGameOver = true;
+
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+
+            dirtParticle.Stop();
+            explosionParticle.Play();
+
+            playerAudio.PlayOneShot(crashSound, 0.5f);
         }
     }
 
